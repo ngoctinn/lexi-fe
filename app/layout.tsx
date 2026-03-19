75701f05e27aa745d31a2136aef5ee75fac30e0b
NgocTin <tinvg2918@gmail.com>
2026-03-19 10:16:18 +0700

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", inter.variable)}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
